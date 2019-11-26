af65ce095ba69a9f18268ae1021a73b8d1a28208
aaron <aaron@mindvalley.com>
2019-11-26 13:45:42 +0800

    implements a basic navbar

--- components/sections/Header.vue
+++ components/sections/Header.vue
@@ -1,10 +1,62 @@
 <template>
-  <h1>Header</h1>
+  <nav class="flex items-center justify-between flex-wrap md:flex-no-wrap p-6">
+    <div class="flex items-center flex-shrink-0 mr-6">
+      <span class="pr-4">LOGO!</span>
+      <span class="font-semibold text-xl tracking-tight">
+        Destination Malaysia
+      </span>
+    </div>
+    <div class="block md:hidden">
+      <button
+        @click="mobileNavbarToggle"
+        class="flex items-center px-3 py-2 border rounded"
+      >
+        <svg
+          class="fill-current h-3 w-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
+    </div>
+    <div
+      :class="{ hidden: mobileNavbarHidden }"
+      class="w-full block flex-grow md:flex md:items-center md:justify-around"
+    >
+      <div v-for="links in ['Docs', 'Examples', 'Blog']" class="text-sm">
+        <a
+          class="block mt-4 lg:inline-block md:mt-0 mr-4"
+          href="#responsive-header"
+        >
+          {{ links }}
+        </a>
+      </div>
+      <div>
+        <a
+          class="inline-block text-sm px-4 py-2 leading-none border rounded mt-4 md:mt-0"
+          href="#"
+          >Download</a
+        >
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data() {
+    return {
+      mobileNavbarHidden: true
+    }
+  },
+  methods: {
+    mobileNavbarToggle() {
+      this.mobileNavbarHidden = !this.mobileNavbarHidden
+    }
+  }
 }
 </script>
 
