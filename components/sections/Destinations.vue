891940375e9667e701a0571bd542ac9db813622b
aaron <aaron@mindvalley.com>
2019-11-29 01:07:53 +0800

    destination and hotel sections

--- components/sections/Destinations.vue
+++ components/sections/Destinations.vue
@@ -1,11 +1,23 @@
 <template>
-  <h1>Destinations</h1>
+  <div class="mt-8">
+    <h2 class="text-center text-3xl font-bold tracking-wide">Destinations</h2>
+    <div class="mx-auto mt-4 flex flex-wrap justify-center xl:w-2/3">
+      <BaseImg v-for="image of 6" :key="image" class="m-1" />
+    </div>
+    <button
+      class="block mx-auto mt-4 px-6 py-2 border-2 rounded-full font-bold hover:bg-blue-200 focus:border-blue-900 focus:rounded-full focus:outline-none"
+    >
+      View More
+    </button>
+  </div>
 </template>
 
 <script>
+import BaseImg from '../base/BaseImg'
 export default {
-  name: 'Destinations'
+  name: 'Destinations',
+  components: {
+    BaseImg
+  }
 }
 </script>
-
-<style scoped></style>
--- components/sections/Hotels.vue
+++ components/sections/Hotels.vue
@@ -1,10 +1,24 @@
 <template>
-  <h1>Hotels</h1>
+  <div class="mt-8">
+    <h2 class="text-center text-3xl font-bold tracking-wide">Hotels</h2>
+    <div class="mx-auto mt-4 flex flex-wrap justify-center xl:w-2/3">
+      <BaseImg v-for="image of 3" :key="image" class="m-1" />
+    </div>
+    <button
+      class="block mx-auto mt-4 px-6 py-2 border-2 rounded-full font-bold hover:bg-blue-200 focus:border-blue-900 focus:rounded-full focus:outline-none"
+    >
+      More Hotels
+    </button>
+  </div>
 </template>
 
 <script>
+import BaseImg from '../base/BaseImg'
 export default {
-  name: 'Hotels'
+  name: 'Hotels',
+  components: {
+    BaseImg
+  }
 }
 </script>
 
