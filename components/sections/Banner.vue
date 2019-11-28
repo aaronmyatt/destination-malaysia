acab29c0c95901192a99d736bbfe852b24d462bf
aaron <aaron@mindvalley.com>
2019-11-28 14:13:05 +0800

    adds placeholder/skeleton banner

--- components/sections/Banner.vue
+++ components/sections/Banner.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>Banner</h1>
+  <div>
+    <img
+      class="mx-auto"
+      src="https://via.placeholder.com/1200x600.png?text=Destination+Malaysia"
+      alt="Destination Malaysia"
+    />
+  </div>
 </template>
 
 <script>
